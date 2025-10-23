54141b44730fbdb6b90519d5c9f276229a4ab19c
massivebird <33991373+massivebird@users.noreply.github.com>
2025-10-22 21:20:47 -0400

    feat: crossterm input kinda

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+use std::io::Write;
+
+use crossterm::event::KeyCode;
+
 use self::{card::Card, deck::Deck};
 
 mod card;
 mod deck;
 
 fn main() {
+    let mut stdout = std::io::stdout();
+
+    crossterm::execute!(stdout, crossterm::cursor::Hide).unwrap();
+    crossterm::terminal::enable_raw_mode().unwrap();
+
     let mut rng = rand::rng();
 
     let mut deck = Deck::generate(&mut rng);
@@ -13,6 +22,21 @@ fn main() {
     // Initial populate
     for slot in &mut room {
         *slot = deck.draw();
+    }
+
+    'outer: loop {
+        for event in crossterm::event::read() {
+            let Some(event) = event.as_key_event() else {
+                println!("con");
+                continue;
+            };
+
+            match event.code {
+                KeyCode::Char('q') => break 'outer,
+                KeyCode::Char('w') => println!("w"),
+                _ => (),
+            }
+        }
     }
 
     dbg!(room);
